2ec4949dcbf3eb3805077e708ded8418c972f77b
Hai Tran <tuanhai.dev@gmail.com>
2020-01-08 14:44:58 +0700

    Create custom username textfield

---
+++ Github/UsernameTextField.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+class UsernameTextField: UITextField {
+  
+  //MARK: - Initializer
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    configure()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  // MARK: - Helper
+  private func configure() {
+    translatesAutoresizingMaskIntoConstraints = false
+    
+    layer.cornerRadius = 10
+    layer.borderWidth = 2
+    layer.borderColor = UIColor.systemGray4.cgColor
+    
+    textColor = .label
+    tintColor = .label
+    textAlignment = .center
+    font = .preferredFont(forTextStyle: .title2)
+    adjustsFontSizeToFitWidth = true
+    minimumFontSize = 12
+    
+    backgroundColor = .tertiarySystemBackground
+    autocorrectionType = .no
+    placeholder = "Enter an username"
+  }
+}
