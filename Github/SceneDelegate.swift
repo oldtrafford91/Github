bac10aa04a932efc94dd35de1e1f2e54be07311d
Hai Tran <tuanhai.dev@gmail.com>
2020-01-08 14:10:44 +0700

    Setup project with tabbar controller

--- Github/SceneDelegate.swift
+++ Github/SceneDelegate.swift
@@ -5,15 +5,31 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   var window: UIWindow?
 
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-    
-    let searchViewController = SearchViewController()
-
-    let tabbarController = UITabBarController()
-    tabbarController.viewControllers = [searchViewController]
-    
     guard let windowScene = (scene as? UIWindowScene) else { return }
     window = UIWindow(windowScene: windowScene)
-    window?.rootViewController = tabbarController
+    window?.rootViewController = createTabbarController()
     window?.makeKeyAndVisible()
+  }
+  
+  //MARK: - Helpers
+  
+  private func createTabbarController() -> UITabBarController{
+    let tabbarController = UITabBarController()
+    tabbarController.viewControllers = [createSearchNavigationController(), createFavoritesNavigationController()]
+    return tabbarController
+  }
+  
+  private func createSearchNavigationController() -> UINavigationController {
+    let searchViewController = SearchViewController()
+    searchViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+    let searchNavigationController = UINavigationController(rootViewController: searchViewController)
+    return searchNavigationController
+  }
+  
+  private func createFavoritesNavigationController() -> UINavigationController {
+    let favoritesViewController = FavoritesViewController()
+    favoritesViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+    let favoritesNavigationController = UINavigationController(rootViewController: favoritesViewController)
+    return favoritesNavigationController
   }
 }
