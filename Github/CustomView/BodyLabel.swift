48122346f8db3e00ca2b5c7a7f2287f683d0f7aa
Hai Tran <tuanhai.dev@gmail.com>
2020-01-10 14:11:24 +0700

    Create custom `BodyLabel` for displaying body label

---
+++ Github/CustomView/BodyLabel.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+class Body: UILabel {
+  
+  //MARK: - Initializer
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    configure()
+  }
+  
+  convenience init(textAligment: NSTextAlignment) {
+    self.init(frame: .zero)
+    self.textAlignment = textAlignment
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  //MARK: - Setup
+  private func configure() {
+    translatesAutoresizingMaskIntoConstraints = false
+    font = UIFont.preferredFont(forTextStyle: .body)
+    textColor = .secondaryLabel
+    adjustsFontSizeToFitWidth = true
+    minimumScaleFactor = 0.75
+    lineBreakMode = .byWordWrapping
+  }
+}
