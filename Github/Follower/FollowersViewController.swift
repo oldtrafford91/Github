2c7fbef83ef156017b02c80d471f1f03cac4bb0a
Hai Tran <tuanhai.dev@gmail.com>
2020-01-10 09:03:47 +0700

    Show navigation bar on `FollowersViewController`. Change background color for smoother navigation animation

--- Github/Follower/FollowersViewController.swift
+++ Github/Follower/FollowersViewController.swift
@@ -1,7 +1,14 @@
 import UIKit
 
 class FollowersViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+  //MARK: - View Life Cycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemBackground
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    navigationController?.isNavigationBarHidden = false
+  }
 }
