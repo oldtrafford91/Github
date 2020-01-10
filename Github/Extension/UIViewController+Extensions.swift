45ec8cddd972422046b8d5c1a3cd84e76d17ad24
Hai Tran <tuanhai.dev@gmail.com>
2020-01-10 15:38:23 +0700

    Change `modalPresentationStyle` to crossOverFullScreen for better UI/UX

--- Github/Extension/UIViewController+Extensions.swift
+++ Github/Extension/UIViewController+Extensions.swift
@@ -4,7 +4,7 @@ extension UIViewController {
   func showAlertOnMainThread(title: String, message: String, buttonTitle: String) {
     DispatchQueue.main.async {
       let alertViewController = AlertViewController(alertTitle: title, message: message, buttonTitle: buttonTitle)
-      alertViewController.modalPresentationStyle = .fullScreen
+      alertViewController.modalPresentationStyle = .overFullScreen
       alertViewController.modalTransitionStyle = .crossDissolve
       self.present(alertViewController, animated: true, completion: nil)
     }
