b90a074d0ed30df0f5b2de19fb8907e3f13601bd
Hai Tran <tuanhai.dev@gmail.com>
2020-01-12 13:44:05 +0700

    Prevent leak by using capture list and guard check

--- Github/Features/Follower/FollowersViewController.swift
+++ Github/Features/Follower/FollowersViewController.swift
@@ -9,7 +9,8 @@ class FollowersViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     configureUI()
-    NetworkClient.shared.get(username: username, page: 1) { (result: Result<[Follower], NetworkError>) in
+    NetworkClient.shared.get(username: username, page: 1) { [weak self] (result: Result<[Follower], NetworkError>) in
+      guard let self = self else { return }
       switch result {
       case .success(let followers):
         print(followers)
