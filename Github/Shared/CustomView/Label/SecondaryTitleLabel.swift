4c3d011e9f8dfd26d301920910597479aaddfa68
Hai Tran <tuanhai.dev@gmail.com>
2020-01-27 14:28:23 +0700

    Create `SecondaryTitleLabel`

--- Github/Shared/CustomView/Label/SecondaryTitleLabel.swift
+++ Github/Shared/CustomView/Label/SecondaryTitleLabel.swift
@@ -1,9 +1,28 @@
-//
-//  SecondaryTitleLabel.swift
-//  Github
-//
-//  Created by Tran Tuan Hai on 2/8/20.
-//  Copyright © 2020 Tran Tuan Hai. All rights reserved.
-//
+import UIKit
 
-import Foundation
+class SecondaryTitleLabel: UILabel {
+  
+  //MARK: - Initializer
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    configure()
+  }
+  
+  convenience init(fontSize: CGFloat) {
+    self.init(frame: .zero)
+    self.font = UIFont.systemFont(ofSize: fontSize, weight: .medium)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  //MARK: - Setup
+  private func configure() {
+    translatesAutoresizingMaskIntoConstraints = false
+    textColor = .secondaryLabel
+    adjustsFontSizeToFitWidth = true
+    minimumScaleFactor = 0.9
+    lineBreakMode = .byTruncatingTail
+  }
+}
