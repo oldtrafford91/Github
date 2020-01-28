da8e3b8032348fc74275f725bef28ae2e2c1de8b
Hai Tran <tuanhai.dev@gmail.com>
2020-01-28 15:29:36 +0700

    Add a covenience init for `AvatarImageView`

--- Github/Shared/CustomView/ImageView/AvatarImageView.swift
+++ Github/Shared/CustomView/ImageView/AvatarImageView.swift
@@ -8,6 +8,10 @@ class AvatarImageView: UIImageView {
     configure()
   }
   
+  convenience init() {
+    self.init(frame: .zero)
+  }
+  
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
