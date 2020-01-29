a12cf3f096f7a5e48a77ae84d81d8920c7855060
Hai Tran <tuanhai.dev@gmail.com>
2020-01-29 19:31:57 +0700

    Layout `UserInfoHeaderViewController` and add it to `UserInfoViewController` as child view controller

--- Github/Modules/UserInfo/UserInfoHeaderViewController.swift
+++ Github/Modules/UserInfo/UserInfoHeaderViewController.swift
@@ -1,16 +1,111 @@
 import UIKit
 
 class UserInfoHeaderViewController: UIViewController {
-  // MARK: - Properties
+  // MARK: Properties
+  var user: User! {
+    didSet {
+      configure(with: user)
+    }
+  }
+  
+  // MARK: - Views
   let avatarImageView = AvatarImageView()
   let usernameLabel = TitleLabel(textAlignment: .left, fontSize: 34)
   let nameLabel = SecondaryTitleLabel(fontSize: 18)
-  
+  let locationImageView = UIImageView()
+  let locationLabel = SecondaryTitleLabel(fontSize: 18)
+  let bioLabel = BodyLabel(textAlignment: .left)
   
   // MARK: - View Life Cycle
-  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    configureViewHierachy()
+  }
   // MARK: - Configure
+  private func configureViewHierachy() {
+    addSubviews()
+    configureAvatarImageView()
+    configureUsernameLabel()
+    configureNameLabel()
+    configureLocationLabel()
+    configureLocationImageView()
+    configureBioLabel()
+  }
   
+  private func addSubviews() {
+    view.addSubview(avatarImageView)
+    view.addSubview(usernameLabel)
+    view.addSubview(nameLabel)
+    view.addSubview(locationImageView)
+    view.addSubview(locationLabel)
+    view.addSubview(bioLabel)
+  }
   
+  private func configureAvatarImageView() {
+    NSLayoutConstraint.activate([
+      avatarImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
+      avatarImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+      avatarImageView.widthAnchor.constraint(equalToConstant: 90),
+      avatarImageView.heightAnchor.constraint(equalToConstant: 90),
+    ])
+  }
+  
+  private func configureUsernameLabel() {
+    NSLayoutConstraint.activate([
+      usernameLabel.topAnchor.constraint(equalTo: avatarImageView.topAnchor),
+      usernameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 12),
+      usernameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+      usernameLabel.heightAnchor.constraint(equalToConstant: 38)
+    ])
+  }
+  
+  private func configureNameLabel() {
+    NSLayoutConstraint.activate([
+      nameLabel.centerYAnchor.constraint(equalTo: avatarImageView.centerYAnchor, constant: 8),
+      nameLabel.leadingAnchor.constraint(equalTo: usernameLabel.leadingAnchor),
+      nameLabel.trailingAnchor.constraint(equalTo: usernameLabel.trailingAnchor),
+      nameLabel.heightAnchor.constraint(equalToConstant: 20)
+    ])
+  }
+  
+  private func configureLocationImageView() {
+    locationImageView.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      locationImageView.bottomAnchor.constraint(equalTo: avatarImageView.bottomAnchor),
+      locationImageView.leadingAnchor.constraint(equalTo: usernameLabel.leadingAnchor),
+      locationImageView.widthAnchor.constraint(equalToConstant: 20),
+      locationImageView.heightAnchor.constraint(equalToConstant: 20)
+    ])
+  }
+  
+  private func configureLocationLabel() {
+    NSLayoutConstraint.activate([
+      locationLabel.leadingAnchor.constraint(equalTo: locationImageView.trailingAnchor, constant: 5),
+      locationLabel.bottomAnchor.constraint(equalTo: avatarImageView.bottomAnchor),
+      locationLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+      locationLabel.heightAnchor.constraint(equalToConstant: 20)
+    ])
+  }
+  
+  private func configureBioLabel() {
+    bioLabel.numberOfLines = 3
+    NSLayoutConstraint.activate([
+      bioLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 12),
+      bioLabel.leadingAnchor.constraint(equalTo: avatarImageView.leadingAnchor),
+      bioLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+      bioLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20)
+    ])
+  }
+  
+  private func configure(with user: User) {
+    DispatchQueue.main.async {
+      self.avatarImageView.setImage(with: user.avatarUrl)
+      self.usernameLabel.text = user.login
+      self.nameLabel.text = user.name
+      self.locationImageView.image = UIImage(systemName: "mappin.and.ellipse")
+      self.locationLabel.text = user.location
+      self.bioLabel.text = user.bio
+    }
+  }
   
 }
--- Github/Modules/UserInfo/UserInfoViewController.swift
+++ Github/Modules/UserInfo/UserInfoViewController.swift
@@ -4,6 +4,10 @@ class UserInfoViewController: UIViewController {
   // MARK: Properties
   var username: String!
   
+  // MARK: Views
+  let headerView = UIView()
+  let headerVC = UserInfoHeaderViewController()
+  
   // MARK: Dependencies
   let viewModel = UserInfoViewModel()
   
@@ -19,21 +23,36 @@ class UserInfoViewController: UIViewController {
   private func configureViewHierachy() {
     view.backgroundColor = .systemBackground
     configureNavigationBar()
+    configureHeaderView()
   }
   
-  func configureNavigationBar() {
+  private func configureNavigationBar() {
     title = username
     let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
     navigationItem.rightBarButtonItem = doneButton
   }
   
-  func configureViewModelBinding() {
+  private func configureHeaderView() {
+    headerView.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(headerView)
+    
+    NSLayoutConstraint.activate([
+      headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+      headerView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+      headerView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+      headerView.heightAnchor.constraint(equalToConstant: 200)
+    ])
+    add(headerVC, in: headerView)
+  }
+  
+  private func configureViewModelBinding() {
     viewModel.onFetchedUser = { [weak self] user in
       guard let self = self else { return }
-      print(user)
+      self.headerVC.user = user
     }
     viewModel.onFetchedUserFailed = { [weak self] error in
-      print(error.localizedDescription)
+      guard let self = self else { return }
+      self.showAlertOnMainThread(title: "Something wrong", message: error.localizedDescription, buttonTitle: "OK")
     }
   }
   
@@ -43,7 +62,7 @@ class UserInfoViewController: UIViewController {
   }
   
   // MARK: Actions
-  func getUserInfo() {
+  private func getUserInfo() {
     viewModel.getUserInfo(username: username)
   }
 }
