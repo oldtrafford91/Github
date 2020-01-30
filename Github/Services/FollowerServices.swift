76fb89d962cb782a34166405f330706b1c5f15cd
Hai Tran <tuanhai.dev@gmail.com>
2020-01-30 22:19:16 +0700

    Move `Services` folder out of `Networking` module

--- Github/Services/FollowerServices.swift
+++ Github/Services/FollowerServices.swift
@@ -1,19 +1,15 @@
 import Foundation
 
-class UserServices {
+class FollowerServices {
   private let client: HTTPClient
   
   init(client: HTTPClient) {
     self.client = client
   }
   
-  func getUserInfo(of user: String, completion: @escaping (Result<User, APIError>) -> Void) {
-    let endpoint = GithubEndpoint.getUserInfo(of: user)
+  func getFollowers(of user: String, limit: Int = 20, page: Int = 1, completion: @escaping (Result<[Follower], APIError>) -> Void) {
+    let endpoint = GithubEndpoint.getFollower(of: user, limit: limit, page: page)
     guard let urlRequest = endpoint.urlRequest else { return }
     client.request(urlRequest, completion: JSONMapper.map(completion: completion))
   }
 }
-
-
-
-
